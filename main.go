2cd951b7f9a884c8f5bbb382d77e0b3518b2ad14
Louis-Henri Merino <lhmerino@umd.edu>
2020-03-21 21:21:36 -0400

    Reassemble TCP stream

--- main.go
+++ main.go
@@ -1,12 +1,98 @@
 package main
 
 import (
+	"encoding/binary"
+	"encoding/hex"
+	"flag"
+	"fmt"
 	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/reassembly"
-	"io"
+	"os"
+	"os/signal"
 	"sync"
 )
 
+// Setup variables
+
+var pcapFile = flag.String("p", "", "PCAP file")
+var iface = flag.String("i", "en0", "Interface to get packets from")
+var snaplen = flag.Int("s", 1600, "SnapLen for pcap packet capture")
+var filter = flag.String("f", "tcp and port 59168", "BPF filter for pcap")
+var logAllPackets = flag.Bool("v", false, "Logs every packet in great detail")
+
+// TCP Options
+var allowmissinginit = flag.Bool("allowmissinginit", false, "Support streams without SYN/SYN+ACK/ACK sequence")
+
+// Logging Parameters
+var hexdump = flag.Bool("dump", true, "Dump HTTP request/response as hex")
+var hexdumppkt = flag.Bool("dumppkt", true, "Dump packet as hex")
+
+var verbose = flag.Bool("verbose", false, "Be verbose")
+var debug = flag.Bool("debug", false, "Display debug information")
+var quiet = flag.Bool("quiet", false, "Be quiet regarding errors")
+
+// --------------------- Logging ------------------
+var outputLevel int
+var errorsMap map[string]uint
+var errorsMapMutex sync.Mutex
+var errors uint
+
+func Error(t string, s string, a ...interface{}) {
+	errorsMapMutex.Lock()
+	errors++
+	nb, _ := errorsMap[t]
+	errorsMap[t] = nb + 1
+	errorsMapMutex.Unlock()
+	if outputLevel >= 0 {
+		fmt.Printf(s, a...)
+	}
+}
+func Info(s string, a ...interface{}) {
+	if outputLevel >= 1 {
+		fmt.Printf(s, a...)
+	}
+}
+func Debug(s string, a ...interface{}) {
+	if outputLevel >= 2 {
+		fmt.Printf(s, a...)
+	}
+}
+
+func setupLogging() {
+	if *debug {
+		outputLevel = 2
+	} else if *verbose {
+		outputLevel = 1
+	} else if *quiet {
+		outputLevel = -1
+	}
+	errorsMap = make(map[string]uint)
+
+}
+
+// ------------------ END Logging ---------------------
+// ------------------ STATS ---------------------------
+/*var stats struct {
+	ipdefrag            int
+	missedBytes         int
+	pkt                 int
+	sz                  int
+	totalsz             int
+	rejectFsm           int
+	rejectOpt           int
+	rejectConnFsm       int
+	reassembled         int
+	outOfOrderBytes     int
+	outOfOrderPackets   int
+	biggestChunkBytes   int
+	biggestChunkPackets int
+	overlapBytes        int
+	overlapPackets      int
+}*/
+
+// ------------------ TCP ASSEMBLER -------------------
 /*
  * TCP stream
  */
@@ -27,30 +113,341 @@ type tcpStream struct {
 	sync.Mutex
 }
 
-/* HTTP */
+func (t *tcpStream) Accept(tcp *layers.TCP, ci gopacket.CaptureInfo, dir reassembly.TCPFlowDirection, nextSeq reassembly.Sequence, start *bool, ac reassembly.AssemblerContext) bool {
+	// FSM
+	if !t.tcpstate.CheckState(tcp, dir) {
+		Error("FSM", "%s: Packet rejected by FSM (state:%s)\n", t.ident, t.tcpstate.String())
+		//stats.rejectFsm++
+		if !t.fsmerr {
+			t.fsmerr = true
+			//stats.rejectConnFsm++
+		}
+		/*if !*ignorefsmerr {
+			return false
+		}*/
+	}
+	// Options
+	err := t.optchecker.Accept(tcp, ci, dir, nextSeq, start)
+	if err != nil {
+		Error("OptionChecker", "%s: Packet rejected by OptionChecker: %s\n", t.ident, err)
+		//stats.rejectOpt++
+		/*if !*nooptcheck {
+			return false
+		}*/
+	}
+	// Checksum
+	accept := true
+	/*if *checksum {
+		c, err := tcp.ComputeChecksum()
+		if err != nil {
+			Error("ChecksumCompute", "%s: Got error computing checksum: %s\n", t.ident, err)
+			accept = false
+		} else if c != 0x0 {
+			Error("Checksum", "%s: Invalid checksum: 0x%x\n", t.ident, c)
+			accept = false
+		}
+	}*/
+	if !accept {
+		//stats.rejectOpt++
+	}
+	return accept
+}
 
+func (t *tcpStream) ReassembledSG(sg reassembly.ScatterGather, ac reassembly.AssemblerContext) {
+	dir, start, end, skip := sg.Info()
+	length, saved := sg.Lengths()
+	// update stats
+	sgStats := sg.Stats()
+	if skip > 0 {
+		//stats.missedBytes += skip
+	}
+	//stats.sz += length - saved
+	//stats.pkt += sgStats.Packets
+	if sgStats.Chunks > 1 {
+		//stats.reassembled++
+	}
+	//stats.outOfOrderPackets += sgStats.QueuedPackets
+	//stats.outOfOrderBytes += sgStats.QueuedBytes
+	//if length > stats.biggestChunkBytes {
+	//stats.biggestChunkBytes = length
+	//}
+	//if sgStats.Packets > stats.biggestChunkPackets {
+	//stats.biggestChunkPackets = sgStats.Packets
+	//}
+	if sgStats.OverlapBytes != 0 && sgStats.OverlapPackets == 0 {
+		fmt.Printf("bytes:%d, pkts:%d\n", sgStats.OverlapBytes, sgStats.OverlapPackets)
+		panic("Invalid overlap")
+	}
+	//stats.overlapBytes += sgStats.OverlapBytes
+	//stats.overlapPackets += sgStats.OverlapPackets
+
+	var ident string
+	if dir == reassembly.TCPDirClientToServer {
+		ident = fmt.Sprintf("%v %v(%s): ", t.net, t.transport, dir)
+	} else {
+		ident = fmt.Sprintf("%v %v(%s): ", t.net.Reverse(), t.transport.Reverse(), dir)
+	}
+	Debug("%s: SG reassembled packet with %d bytes (start:%v,end:%v,skip:%d,saved:%d,nb:%d,%d,overlap:%d,%d)\n", ident, length, start, end, skip, saved, sgStats.Packets, sgStats.Chunks, sgStats.OverlapBytes, sgStats.OverlapPackets)
+	if skip == -1 && *allowmissinginit {
+		// this is allowed
+	} else if skip != 0 {
+		// Missing bytes in stream: do not even try to parse it
+		return
+	}
+	data := sg.Fetch(length)
+	if t.isDNS {
+		dns := &layers.DNS{}
+		var decoded []gopacket.LayerType
+		if len(data) < 2 {
+			if len(data) > 0 {
+				sg.KeepFrom(0)
+			}
+			return
+		}
+		dnsSize := binary.BigEndian.Uint16(data[:2])
+		missing := int(dnsSize) - len(data[2:])
+		Debug("dnsSize: %d, missing: %d\n", dnsSize, missing)
+		if missing > 0 {
+			Info("Missing some bytes: %d\n", missing)
+			sg.KeepFrom(0)
+			return
+		}
+		p := gopacket.NewDecodingLayerParser(layers.LayerTypeDNS, dns)
+		err := p.DecodeLayers(data[2:], &decoded)
+		if err != nil {
+			Error("DNS-parser", "Failed to decode DNS: %v\n", err)
+		} else {
+			Debug("DNS: %s\n", gopacket.LayerDump(dns))
+		}
+		if len(data) > 2+int(dnsSize) {
+			sg.KeepFrom(2 + int(dnsSize))
+		}
+	} else if t.isHTTP {
+		if length > 0 {
+			if *hexdump {
+				Debug("Feeding http with:\n%s", hex.Dump(data))
+			}
+			if dir == reassembly.TCPDirClientToServer && !t.reversed {
+				t.client.bytes <- data
+			} else {
+				t.server.bytes <- data
+			}
+		}
+	}
+}
+
+func (t *tcpStream) ReassemblyComplete(ac reassembly.AssemblerContext) bool {
+	Debug("%s: Connection closed\n", t.ident)
+	if t.isHTTP {
+		close(t.client.bytes)
+		close(t.server.bytes)
+	}
+	// do not remove the connection to allow last ACK
+	return false
+}
+
+type tcpStreamFactory struct {
+	wg     sync.WaitGroup
+	doHTTP bool
+}
+
+func (factory *tcpStreamFactory) New(net, transport gopacket.Flow, tcp *layers.TCP, ac reassembly.AssemblerContext) reassembly.Stream {
+	Debug("* NEW: %s %s\n", net, transport)
+	fsmOptions := reassembly.TCPSimpleFSMOptions{
+		SupportMissingEstablishment: *allowmissinginit,
+	}
+	stream := &tcpStream{
+		net:        net,
+		transport:  transport,
+		isDNS:      tcp.SrcPort == 53 || tcp.DstPort == 53,
+		isHTTP:     (tcp.SrcPort == 80 || tcp.DstPort == 80) && factory.doHTTP,
+		reversed:   tcp.SrcPort == 80,
+		tcpstate:   reassembly.NewTCPSimpleFSM(fsmOptions),
+		ident:      fmt.Sprintf("%s:%s", net, transport),
+		optchecker: reassembly.NewTCPOptionCheck(),
+	}
+	/*if stream.isHTTP {
+		stream.client = httpReader{
+			bytes:    make(chan []byte),
+			ident:    fmt.Sprintf("%s %s", net, transport),
+			hexdump:  *hexdump,
+			parent:   stream,
+			isClient: true,
+		}
+		stream.server = httpReader{
+			bytes:   make(chan []byte),
+			ident:   fmt.Sprintf("%s %s", net.Reverse(), transport.Reverse()),
+			hexdump: *hexdump,
+			parent:  stream,
+		}
+		factory.wg.Add(2)
+		go stream.client.run(&factory.wg)
+		go stream.server.run(&factory.wg)
+	}*/
+	return stream
+}
+
+func (factory *tcpStreamFactory) WaitGoRoutines() {
+	factory.wg.Wait()
+}
+
+// ------------------ END TCP ASSEMBLER ---------------
+// ------------------ HTTP ASSEMBLER ------------------
 type httpReader struct {
-	ident   string
-	bytes   chan []byte
-	data    []byte
-	hexdump bool
-	parent  *tcpStream
+	ident    string
+	isClient bool
+	bytes    chan []byte
+	data     []byte
+	hexdump  bool
+	parent   *tcpStream
+}
+
+/*type httpStream struct {
+	net, transport gopacket.Flow
+	r              tcpreader.ReaderStream
+}
+
+type httpStreamFactory struct{}
+
+func (h *httpStreamFactory) New(net, transport gopacket.Flow) tcpassembly.Stream {
+	hstream := &httpStream{
+		net:       net,
+		transport: transport,
+		r:         tcpreader.NewReaderStream(),
+	}
+	go hstream.run() // Important... we must guarantee that data from the reader stream is read.
+
+	// ReaderStream implements tcpassembly.Stream, so we can return a pointer to it.
+	return &hstream.r
+}
+
+func (h *httpStream) run() {
+	buf := bufio.NewReader(&h.r)
+	for {
+		req, err := http.ReadRequest(buf)
+		if err == io.EOF {
+			// We must read until we see an EOF... very important!
+			return
+		} else if err != nil {
+			//log.Println("Error reading stream", h.net, h.transport, ":", err)
+		} else {
+			//bodyBytes := tcpreader.DiscardBytesToEOF(req.Body)
+			req.Body.Close()
+			//log.Println("Received request from stream", h.net, h.transport, ":", req, "with", bodyBytes, "bytes in request body")
+		}
+	}
+}*/
+
+// ------------------ FLOWS ---------------------------
+
+type FlowData struct {
+	flow  gopacket.Flow // provided by GoPacket
+	flags uint8         // use one of the 8 flags to mark an occurred event
+}
+
+// Our hashmap
+var flows = make(map[uint64]FlowData)
+
+// ------------------ END FLOWS -----------------------
+
+/*func runLiveCapture(handle *pcap.Handle, assembler tcpassembly.Assembler) {
+	Info("reading in packets")
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packets := packetSource.Packets()
+	ticker := time.Tick(time.Minute)
+
+	for {
+		select {
+		case packet := <-packets:
+			// A nil packet indicates the end of a pcap file.
+			if packet == nil {
+				return
+			}
+			if *logAllPackets {
+				Info(packet.String())
+			}
+			if packet.NetworkLayer() == nil || packet.TransportLayer() == nil || packet.TransportLayer().LayerType() != layers.LayerTypeTCP {
+				Info("Unusable packet")
+				continue
+			}
+			//tcp := packet.TransportLayer().(*layers.TCP)
+			//assembler.AssembleWithTimestamp(packet.NetworkLayer().NetworkFlow(), tcp, packet.Metadata().Timestamp)
+
+		case <-ticker:
+			// Every minute, flush connections that haven't seen activity in the past 2 minutes.
+			assembler.FlushOlderThan(time.Now().Add(time.Minute * -2))
+		}
+	}
+}*/
+
+/*
+ * The assembler context
+ */
+type Context struct {
+	CaptureInfo gopacket.CaptureInfo
+}
+
+func (c *Context) GetCaptureInfo() gopacket.CaptureInfo {
+	return c.CaptureInfo
 }
 
 func main() {
+	var err error
+	var handle *pcap.Handle
+	flag.Parse()
 
-}
+	setupLogging()
 
-func (h *httpReader) Read(p []byte) (int, error) {
-	ok := true
-	for ok && len(h.data) == 0 {
-		h.data, ok = <-h.bytes
+	if *pcapFile != "" {
+		Info("Read from pcap: %q\n", *pcapFile)
+		handle, err = pcap.OpenOffline(*pcapFile)
+	} else {
+		Info("Starting capture on interface %q\n", *iface)
+		handle, err = pcap.OpenLive(*iface, int32(*snaplen), true, pcap.BlockForever)
 	}
-	if !ok || len(h.data) == 0 {
-		return 0, io.EOF
+
+	if err != nil {
+		Error("Capture Handle", "Handle open failure: %s (%v,%+v)", err, err, err)
+		return
 	}
 
-	l := copy(p, h.data)
-	h.data = h.data[l:]
-	return l, nil
+	defer handle.Close()
+
+	// Filter packets given filter argument
+	if err := handle.SetBPFFilter(*filter); err != nil {
+		//Error("%s", err)
+	}
+
+	// Set up assembly
+	streamFactory := &tcpStreamFactory{}
+	streamPool := reassembly.NewStreamPool(streamFactory)
+	assembler := reassembly.NewAssembler(streamPool)
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	count := 0
+
+	for packet := range packetSource.Packets() {
+		count++
+
+		data := packet.Data()
+		Debug("Packet #%d content (%d/0x%x)\n%s\n", count, len(data), len(data), hex.Dump(data))
+
+		// Ignore IPv4 de-fragmentation for the time being TODO
+
+		tcp := packet.Layer(layers.LayerTypeTCP)
+
+		if tcp != nil {
+			tcp := tcp.(*layers.TCP)
+
+			c := Context{
+				CaptureInfo: packet.Metadata().CaptureInfo,
+			}
+			assembler.AssembleWithContext(packet.NetworkLayer().NetworkFlow(), tcp, &c)
+		}
+	}
+
 }
